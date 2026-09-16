d9b4b1cc534109cef0bccda9d1718f45d470d522
Tamal Chowdhury <tamalanwaronline@gmail.com>
2026-09-16 18:28:37 +0600

    fix req type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import {
   withAuth,
 } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import type { NextRequest } from "next/server";
 
 export default withAuth(
-  async function middleware(req) {
+  async function middleware(req: NextRequest) {
   },
   {
     publicPaths: ["/", "/api/public"],
